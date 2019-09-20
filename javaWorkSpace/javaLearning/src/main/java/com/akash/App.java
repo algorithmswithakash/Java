ab9d5687ef768e3f44e0ccac84a4cb38fa48c98a
Akash Agarwal <aagarwal@sonicwall.com>
2019-09-20 08:12:48 -0700

    Included Maven

---
+++ javaWorkSpace/javaLearning/src/main/java/com/akash/App.java
@@ -0,0 +1,13 @@
+package com.akash;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
